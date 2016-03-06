1f6ad306a184bfe1780bd70f718a8e12ae1c150f
Yoshi <ymurakami2@my.bcit.ca>
2016-03-06 13:26:55 -0800

    modified

--- scripts/vgvScript.js
+++ scripts/vgvScript.js
@@ -2,7 +2,37 @@ function $(id){
 	var element = document.getElementById(id);
 	if(element == null)
 		alert('Programmer error: ' + id + ' does not exist.');
-	//return element;
-	alert(element);
+	return element;
 }
 
+function nameValid(id){
+	var userName = $(id).value.search(/([.*+?^=!:${}()|[\]\/\\])/);
+	if(userName == 0)	
+		alert("You can use only alphabets or numbers");
+}
+
+function passValid(id){
+	var passLength = $(id).value.length;
+	var passWord = $(id).value.search(/([a-z][A-Z][0-9])/);
+	if(passLength < 6)
+		alert("Password must be over 7 characters.");
+	if(passWord < 0)
+		//alert("Password must be included at least one lower case letter, upper case letter, and number");
+		alert(passWord);
+	
+}
+
+function passChecker(firstId, secondId){
+	var firstPass = $(firstId).value;
+	var secondPass = $(secondId).value;
+	var check = firstPass.search(firstPass);
+	if(check <= 0)
+		alert("Password is not much");
+	
+	
+}
+
+function emailValid(id){
+	var email = $(id).value.search(/^(([a-zA-Z]|[0-9])|([-]|[_]|[.]))+[@](([a-zA-Z0-9])|([-])){2,63}[.](([a-zA-Z0-9]){2,63})+$/gi);
+	alert(email);
+}
