ab5a0e3e83fe1e56337cc8c3a4cbed8a80c71a6f
InochiTanaka <yunominosencha@gmail.com>
2016-03-27 18:10:56 -0700

    accpet

---
+++ scripts/_vti_cnf/vgvScript.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|27 Mar 2016 22:36:54 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Group-Project/register.html Group-Project/index.html Group-Project/staffandpolicy.html Group-Project/catalogue.html Group-Project/news.html Group-Project/thankyou.html Group-Project/content.html
